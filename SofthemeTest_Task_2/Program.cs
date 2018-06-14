89a5f85d69f0fa8b28e33f0cf9fb688f32dd2623
behappyall <not.happy.be.happy@gmail.com>
2018-06-15 01:09:48 +0300

    Task_2

--- SofthemeTest_Task_2/Program.cs
+++ SofthemeTest_Task_2/Program.cs
@@ -10,6 +10,40 @@ namespace SofthemeTest_Task_2
     {
         static void Main(string[] args)
         {
+            //Зчитування
+            Console.Write("Enter number: ");
+            
+            if (!int.TryParse(Console.ReadLine(), out int number))
+                throw new ArgumentException("You must enter digit value", nameof(number));
+
+
+            //получити дільники
+            var dividers = GetDividers(number);
+
+            //Вивід
+            Console.WriteLine($"Dividers of number {number}:");
+
+            dividers.ForEach(Console.WriteLine);
         }
+        static List<int> GetDividers(int number)
+        {
+            if (number < 1)
+                return new List<int>();
+
+            List<int> dividers = new List<int>();
+
+            dividers.Add(1);
+            dividers.Add(number);
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+                if (number % i == 0)
+                {
+                    dividers.Add(i);                    //добавляємо дільник і його доповнення
+                    dividers.Add(number / i);           //добавляємо дільник і його доповнення
+                }
+
+            return dividers.Distinct().OrderBy(i=>i).ToList();
+        }
+
     }
 }
